156cfe673a33a7180a525ef7004fa78866e7b540
Michael Henry <drmikehenry@drmikehenry.com>
2023-02-25 08:54:21 -0500

    Update experiments for bundle speed testing.

--- experiments/bundle-all-git/src/main.rs
+++ experiments/bundle-all-git/src/main.rs
@@ -4,11 +4,18 @@ fn vec_push_oid(v: &mut Vec<u8>, oid: git2::Oid) {
     v.extend_from_slice(&oid.to_string().as_str().as_bytes());
 }
 
-fn push_ref(v: &mut Vec<u8>, commit_id: git2::Oid, name: &[u8]) {
-    vec_push_oid(v, commit_id);
-    v.push(b' ');
-    v.extend_from_slice(name);
-    v.push(b'\n');
+fn push_ref(
+    header: &mut Vec<u8>,
+    pack_input: &mut Vec<u8>,
+    commit_id: git2::Oid,
+    name: &[u8],
+) {
+    vec_push_oid(header, commit_id);
+    header.push(b' ');
+    header.extend_from_slice(name);
+    header.push(b'\n');
+    vec_push_oid(pack_input, commit_id);
+    pack_input.push(b'\n');
 }
 
 fn main() -> anyhow::Result<()> {
@@ -16,31 +23,41 @@ fn main() -> anyhow::Result<()> {
     let repo = git2::Repository::open(repo_path)?;
     let head = repo.resolve_reference_from_short_name("HEAD")?;
 
-    let mut pack_v = Vec::new();
-    let mut v = Vec::new();
-    v.extend_from_slice(b"# v2 git bundle\n");
+    let mut pack_input = Vec::new();
+    let mut header = Vec::new();
+    header.extend_from_slice(b"# v2 git bundle\n");
     for r in repo.references()? {
         let r = r?;
         let commit = r.peel_to_commit()?;
         let commit_id = commit.id();
-        push_ref(&mut v, commit_id, r.name_bytes());
-        vec_push_oid(&mut pack_v, commit_id);
+        push_ref(&mut header, &mut pack_input, commit_id, r.name_bytes());
     }
-    push_ref(&mut v, head.peel_to_commit()?.id(), b"HEAD");
+    push_ref(
+        &mut header,
+        &mut pack_input,
+        head.peel_to_commit()?.id(),
+        b"HEAD",
+    );
 
-    v.push(b'\n');
+    header.push(b'\n');
 
     let mut f = std::fs::File::create("output.bundle")?;
-    f.write_all(&v)?;
+    f.write_all(&header)?;
 
     let mut child = std::process::Command::new("git")
-        .args(["pack-objects", "--stdout", "--thin", "--delta-base-offset"])
+        .args([
+            "pack-objects",
+            "--stdout",
+            "--thin",
+            "--delta-base-offset",
+            "--threads=1",
+        ])
         .stdin(std::process::Stdio::piped())
         .stdout(f)
         .spawn()?;
 
     let mut child_stdin = child.stdin.take().unwrap();
-    child_stdin.write_all(&pack_v)?;
+    child_stdin.write_all(&pack_input)?;
     drop(child_stdin);
 
     let exit_status = child.wait()?;
--- experiments/bundle-all/src/main.rs
+++ experiments/bundle-all/src/main.rs
@@ -1,8 +1,8 @@
-fn push_ref(v: &mut Vec<u8>, commit_id: git2::Oid, name: &[u8]) {
-    v.extend_from_slice(commit_id.to_string().as_str().as_bytes());
-    v.push(b' ');
-    v.extend_from_slice(name);
-    v.push(b'\n');
+fn push_ref(bundle_data: &mut Vec<u8>, commit_id: git2::Oid, name: &[u8]) {
+    bundle_data.extend_from_slice(commit_id.to_string().as_str().as_bytes());
+    bundle_data.push(b' ');
+    bundle_data.extend_from_slice(name);
+    bundle_data.push(b'\n');
 }
 
 fn main() -> anyhow::Result<()> {
@@ -10,20 +10,20 @@ fn main() -> anyhow::Result<()> {
     let repo = git2::Repository::open(repo_path)?;
     let head = repo.resolve_reference_from_short_name("HEAD")?;
     let mut pack = repo.packbuilder()?;
-    pack.set_threads(16);
+    pack.set_threads(1);
     let mut walk = repo.revwalk()?;
-    let mut v = Vec::new();
-    v.extend_from_slice(b"# v2 git bundle\n");
+    let mut bundle_data = Vec::new();
+    bundle_data.extend_from_slice(b"# v2 git bundle\n");
     for r in repo.references()? {
         let r = r?;
         let commit = r.peel_to_commit()?;
         let commit_id = commit.id();
         walk.push(commit_id)?;
-        push_ref(&mut v, commit_id, r.name_bytes());
+        push_ref(&mut bundle_data, commit_id, r.name_bytes());
     }
-    push_ref(&mut v, head.peel_to_commit()?.id(), b"HEAD");
+    push_ref(&mut bundle_data, head.peel_to_commit()?.id(), b"HEAD");
 
-    v.push(b'\n');
+    bundle_data.push(b'\n');
 
     pack.set_progress_callback(|stage, n, m| {
         println!("{:?} {} {}", stage, n, m);
@@ -31,9 +31,10 @@ fn main() -> anyhow::Result<()> {
     })?;
     pack.insert_walk(&mut walk)?;
     pack.foreach(|buf| {
-        v.extend_from_slice(buf);
+        bundle_data.extend_from_slice(buf);
         true
     })?;
-    std::fs::write("output.bundle", &v)?;
+    println!("pack.foreach() complete; writing bundle_data");
+    std::fs::write("output.bundle", &bundle_data)?;
     Ok(())
 }
